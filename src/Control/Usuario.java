da698818a773f1e174ba2c07d53cf89a8fcaf3ac
brahianf <brahianf@unicauca.edu.co>
2020-02-08 21:03:01 -0500

    Atributos y constructor Usuario.java

--- src/Control/Usuario.java
+++ src/Control/Usuario.java
@@ -5,10 +5,23 @@
  */
 package Control;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Antonella
  */
 public class Usuario {
+    
+    public String nombre;
+    public String usuario;
+    public String passwd;
+    public ArrayList <String> listUsuarios = new ArrayList<>();
+    
+    public Usuario(String nombre,String usuario, String pass){
+        this.nombre=nombre;
+        this.usuario=usuario;
+        this.passwd=pass;
+    }
     
 }
